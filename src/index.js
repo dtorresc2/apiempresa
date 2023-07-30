127c6a680bce33ec8a32365f7f314499ccf2b3d5
dtorresc2 <diegotorres_umg@hotmail.com>
2023-07-30 12:53:38 -0600

    Ajustando puerto y .yml 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hola a todos estoy en la version 2!')
 })
 
 app.listen(app.get('port'), () => {
