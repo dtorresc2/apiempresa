3f3aa36d7a69097cfc61674cf5c47f01de0a3aa7
dtorresc2 <diegotorres_umg@hotmail.com>
2023-08-12 20:46:05 -0600

    Variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
+const dotenv = require('dotenv');
+
+// Configuracion archivo - variables de entorno
+const envFile = "./src/config/.env";
+dotenv.config({ path: envFile });
+
+dotenv.config();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hola a todos estoy en la version 2!')
+  res.send(`Variable de entorno ${process.env.DB_HOST}`);
 })
 
 app.listen(app.get('port'), () => {
